8d721d7059a3316952d815b686f2eba62a6530d7
AndresCespedes23 <andres.pablo.cespedes@gmail.com>
2022-06-21 15:37:04 -0300

    added routes to the server

--- build/index.js
+++ build/index.js
@@ -5,12 +5,45 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+const mongoose_1 = __importDefault(require("mongoose"));
+const cors_1 = __importDefault(require("cors"));
+const Cards_1 = __importDefault(require("./Schema/Cards"));
 const app = (0, express_1.default)();
-const port = process.env.PORT;
-app.get('/', (_req, res) => {
-    res.send('Hello World!');
+app.use(express_1.default.json());
+app.use((0, cors_1.default)());
+dotenv_1.default.config();
+mongoose_1.default
+    .connect(process.env.DATABASE_URL)
+    .then(() => {
+    console.log("🟢 DB Connected");
+    app.listen({ port: process.env.PORT }, () => {
+        console.log(`🏃‍♂️ Server running on port ${process.env.PORT}`);
+    });
+})
+    .catch((err) => {
+    console.log("🔴 There was an error on the DB connection method.");
+    console.log(err);
 });
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+// Routes
+app.get("/", (_req, res) => res.status(200).send("Hello World!"));
+app.post("/dating/cards", (req, res) => {
+    const dbCard = req.body;
+    Cards_1.default.create(dbCard, (err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.status(201).send(data);
+        }
+    });
+});
+app.get("/dating/cards", (_req, res) => {
+    Cards_1.default.find((err, data) => {
+        if (err) {
+            res.status(500).send(err);
+        }
+        else {
+            res.status(200).send(data);
+        }
+    });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,50 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response, ErrorRequestHandler } from 'express';
 import dotenv from 'dotenv';
-
-dotenv.config();
+import mongoose from 'mongoose';
+import cors from 'cors';
+import Cards from "./Schema/Cards";
 
 const app: Express = express();
-const port = process.env.PORT;
+app.use(express.json());
+app.use(cors());
+dotenv.config();
 
-app.get('/', (_req: Request, res: Response) => {
-  res.send('Hello World!');
-});
+mongoose
+  .connect(
+    process.env.DATABASE_URL,
+  )
+  .then(() => {
+    console.log("🟢 DB Connected");
+    app.listen({ port: process.env.PORT }, () => {
+      console.log(`🏃‍♂️ Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("🔴 There was an error on the DB connection method.");
+    console.log(err);
+  });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-});
+// Routes
+
+  app.get("/", (_req, res) => res.status(200).send("Hello World!"));
+
+  app.post("/dating/cards", (req: Request, res: Response) => {
+    const dbCard = req.body;
+    Cards.create(dbCard, (err: ErrorRequestHandler, data: String) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(201).send(data);
+      }
+    });
+  });
+  
+  app.get("/dating/cards", (_req: Request, res: Response) => {
+    Cards.find((err: ErrorRequestHandler, data: String) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    });
+  });
